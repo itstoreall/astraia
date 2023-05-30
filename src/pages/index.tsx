19c52629d3ca178176e19dfb51eb12153ef349ed
Serhii Stanislav <serhiistanislav@gmail.com>
2023-05-30 20:24:01 +0300

    ADD | initial files

---
+++ src/pages/index.tsx
@@ -0,0 +1,35 @@
+import Head from 'next/head';
+import Image from 'next/image';
+import { Inter } from 'next/font/google';
+import styles from '@/styles/Home.module.css';
+
+const inter = Inter({ subsets: ['latin'] });
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Astraia</title>
+        <meta name='description' content='Astraia - духовное саморазвитие' />
+        <meta name='viewport' content='width=device-width, initial-scale=1' />
+        <link rel='icon' href='/favicon.ico' />
+      </Head>
+      <main className={`${styles.main} ${inter.className}`}>
+        <div className={styles.description}></div>
+
+        <div className={styles.center}>
+          <Image
+            className={styles.logo}
+            src='/next.svg'
+            alt='Next.js Logo'
+            width={180}
+            height={37}
+            priority
+          />
+        </div>
+
+        <div className={styles.grid}></div>
+      </main>
+    </>
+  );
+}
