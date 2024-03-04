4756c7e7299b68fbd31ed3113140d2e2b77cb80a
Serhii Stanislav <serhiistanislav@gmail.com>
2024-03-04 20:44:08 +0200

    EDIT | code refactoring

--- src/components/Pages/Articles/Sidebar.tsx
+++ src/components/Pages/Articles/Sidebar.tsx
@@ -1,13 +1,24 @@
 import s from './Articles.module.scss';
 
+const categories = [
+  'Тонкий мир',
+  'Нумерология',
+  'Практики',
+  'Эзотерические фильмы',
+  'Болталка'
+];
+
 const Sidebar = () => {
   return (
     <aside className={s.sidebar}>
       <div className={s.sidebarContent}>
-        <span>Нумерология</span>
-        <span>Тонкий мир</span>
-        <span>Магия</span>
-        <span>Єзотерика</span>
+        <ul className={s.sidebarList}>
+          {categories.map((el, idx) => (
+            <li key={idx} className={s.sidebarItem}>
+              <span className={s.itemText}>{el}</span>
+            </li>
+          ))}
+        </ul>
       </div>
     </aside>
   );
